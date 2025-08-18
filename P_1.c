21ec98ae344f22c376cb3152ea9f428382cbecad
gohilparikshitsinh48 <gohilparikshitsinh48@gmail.com>
2025-08-18 22:52:47 +0530

    Create P_1.c

---
+++ P_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+int b;
+
+printf("Enter value of a: ");
+scanf("%d", &a);
+
+printf("Enter value of b: ");
+
+if(a<=b)
+{
+
+printf("a is smallest number");
+ }
+else
+{ 
+
+printf("b is smallest number");
+
+}
+
+return 0;
+}
